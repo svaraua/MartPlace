255ac07b1e4cb0fe538453050cf47b7db852ac2f
svaraua <svarnik7@gmail.com>
2020-04-21 10:40:00 +0300

    final fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function(){
 
+    $(".rating").rate({
+        max_value: 5,
+        step_size: 0.1,
+        readonly: true
+    });
 
-    var mixer = mixitup('.products__inner');
-
+    let mix = $('.products__inner');
+    if (mix) {
+        var mixer = mixitup('.products__inner');
+    }
+    
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
+        
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -33,7 +34,8 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rater/rater.min.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
