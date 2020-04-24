ee45bc9f81fdf103ba221d3f1f93e6f1c1ae2733
svaraua <svarnik7@gmail.com>
2020-04-24 14:24:16 +0300

    added section slider

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,13 @@ $(function(){
         var mixer = mixitup('.products__inner');
     }
     
-
+    $('.slider__inner').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+      });
+        
 });
